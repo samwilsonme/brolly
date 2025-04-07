b27af9a8c0494d4287a0c0aa0bec658af9a74240
Andrada <117421285+ndrada@users.noreply.github.com>
2025-04-06 19:35:24 -0700

    Reorganize structure, added search bar, handled page navigation

---
+++ src/pages/LandingSearch.jsx
@@ -0,0 +1,16 @@
+import SearchBar from '../components/SearchBar';
+import './LandingSearch.css';
+
+export function LandingSearch(){
+    return (
+        <>
+            <div className="header-container">
+            <header className="app-header">
+            <h1>brolly</h1>
+            <p>Do you need an umbrella today?</p>
+            </header>
+            </div>
+            <SearchBar />
+        </>
+    )
+}
