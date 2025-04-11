eb18bb4da3dc885bc792d3a783caf689e4b57b1b
Sam Wilson <samwilsonme@gmail.com>
2025-04-11 06:35:50 +0100

    fix: Condition data needed for icons and expected weather message

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -4,12 +4,11 @@ import getWeatherIcon from "../utils/getWeatherIcons";
 // It takes uses data from a API and displays the weather condition and temperature
 function Weather({time, condition, temperature}) {
   const icon = getWeatherIcon(condition);
-
+  
   return (
     <div className="weather">
       {time && <h3>{time}</h3>}
       <img src={icon} alt={condition} />
-      <p>{condition}</p>
       <p>{Math.round(temperature)}°C</p>
     </div>
   );
