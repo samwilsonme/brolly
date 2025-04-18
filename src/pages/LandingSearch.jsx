1856b4a256a401c3a0a1210a7e7f7c10a50b07df
Sam Wilson <samwilsonme@gmail.com>
2025-04-18 09:21:08 +0100

    style: HTML/CSS Overhual - Part 3

--- src/pages/LandingSearch.jsx
+++ src/pages/LandingSearch.jsx
@@ -1,16 +1,16 @@
 import SearchBar from "../components/SearchBar";
 import "./LandingSearch.css";
+import logo from '../assets/logo/brolly-question.svg'
 
 export function LandingSearch() {
   return (
-    <>
-      <div className="header-container">
-        <header className="app-header">
-          <h1>brolly</h1>
-          <p>Do you need an umbrella today?</p>
-        </header>
-      </div>
+    <main className="search-page">
+      <header>
+        <h1>Brolly: Get Your Local UK Weather Forecast and Umbrella Guidance</h1>
+        <p>Do you need an umbrella today?</p>
+        <img src={logo} alt="brolly" />
+      </header>
       <SearchBar />
-    </>
+    </main>
   );
 }
